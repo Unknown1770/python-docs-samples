d7039bd9dff8290640721f04800f224c3d330236
Vijayant Prateek <62292932+Unknown1770@users.noreply.github.com>
2021-03-16 20:22:57 +0530

    Update main.py

--- appengine/standard_python3/hello_world/main.py
+++ appengine/standard_python3/hello_world/main.py
@@ -1,37 +1,21 @@
-# Copyright 2018 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# importing Flask and other modules 
+from flask import Flask, request, render_template 
 
-# [START gae_python38_app]
-# [START gae_python3_app]
-from flask import Flask
+# Flask constructor 
+app = Flask(__name__) 
 
+# A decorator used to tell the application 
+# which URL is associated function 
+@app.route('/', methods =["GET", "POST"]) 
+def gfg(): 
+	if request.method == "POST": 
+	# getting input with name = fname in HTML form 
+	first_name = request.form.get("fname") 
+	# getting input with name = lname in HTML form 
+	last_name = request.form.get("lname") 
+	return "Your name is "+first_name + last_name 
+	return render_template("form.html") 
 
-# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
-# called `app` in `main.py`.
-app = Flask(__name__)
+if __name__=='__main__': 
+    app.run() 
 
-
-@app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. This
-    # can be configured by adding an `entrypoint` to app.yaml.
-    app.run(host='127.0.0.1', port=8080, debug=True)
-# [END gae_python3_app]
-# [END gae_python38_app]
